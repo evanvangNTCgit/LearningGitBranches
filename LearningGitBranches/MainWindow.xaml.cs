13a71dc17658581dcff9b5fec3108e27e8e34e55
Evan Vang <evankvang@gmail.com>
2025-09-19 19:26:44 -0500

    Add project files.

---
+++ LearningGitBranches/MainWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LearningGitBranches
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void firstCommitButton_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("This button was made on the first commit by Evan Vang when he was learning about git branches!!");
+        }
+    }
+}
